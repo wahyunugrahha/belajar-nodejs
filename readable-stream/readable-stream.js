6bd1e81ed7ba4e883c5241a092cc3e451cbed479
wahyunugrahha <wahyunih297@gmail.com>
2025-05-19 23:58:40 +0700

    Materi Lagi

---
+++ readable-stream/readable-stream.js
@@ -0,0 +1,14 @@
+const { error } = require("console");
+const fs = require("fs");
+
+const readableStream = fs.createReadStream("./article.txt", {
+  highWaterMark: 10,
+});
+
+readableStream.on("readable", () => {
+  try {
+    process.stdout.write(`[${readableStream.read()}]`);
+  } catch (error) {
+    console.error(error);
+  }
+});
